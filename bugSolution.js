9348c5b7f553202c3c64c6d6eaedec1231eb8e62
BugHunter-X <askzenai@gmail.com>
2025-01-24 09:22:04 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,11 @@
+function foo(a, b) {
+  // Explicit type conversion using Number()
+  return Number(a) + Number(b);
+}
+console.log(foo(1, "2")); // Output: 3
+
+// Alternative solution using parseInt()
+function foo2(a, b) {
+  return parseInt(a) + parseInt(b);
+}
+console.log(foo2(1, "2")); // Output: 3
